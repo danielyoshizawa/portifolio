caf9226344ad70af03136014e02329dea1bc19d8
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-06-01 11:44:04 -0300

    Creating the navbar - #2

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>Hello Portifolio!</h1>
     </>
   );
