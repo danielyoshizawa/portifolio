d0b5dd750e35c152d96b59908d1cd3db5e3436af
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-07-28 19:31:29 -0300

    Footer design - #33

--- site/src/components/footer/Footer.js
+++ site/src/components/footer/Footer.js
@@ -2,33 +2,37 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div>
-      <div>
-        <div className="footer-section">
-          <h2>Section</h2>
-          <ul>
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#diplomas">Diplomas</a></li>
-            <li><a href="#certifications">Certifications</a></li>
-          </ul>
+    <div className="footer-background">
+      <div className="footer">
+        <div className="footer-columns">
+          <div className="footer-section">
+            <h2>Section</h2>
+            <ul>
+              <li><a href="#about">About</a></li>
+              <li><a href="#experience">Experience</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#diplomas">Diplomas</a></li>
+              <li><a href="#certifications">Certifications</a></li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h2>Social</h2>
+            <ul>
+              <li><a href="https://linkedin.com/in/danielyoshizawa">Linkedin</a></li>
+              <li><a href="https://www.github.com/danielyoshizawa">Github</a></li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h2>Contact</h2>
+            <ul>
+              <li><a href="mailto:yoshidanielcwb@gmail.com">Email</a></li>
+            </ul>
+          </div>
         </div>
-        <div className="footer-section">
-          <h2>Social</h2>
-          <ul>
-            <li><a href="https://linkedin.com/in/danielyoshizawa">Linkedin</a></li>
-            <li><a href="https://www.github.com/danielyoshizawa">Github</a></li>
-          </ul>
-        </div>
-        <div className="footer-section">
-          <h2>Contact</h2>
-          <ul>
-            <li><a href="mailto:yoshidanielcwb@gmail.com">Email</a></li>
-          </ul>
+        <div className="footer-message">
+          <p>Hacked by Donuts'n'Legends Co - Daniel Yoshizawa</p>
         </div>
       </div>
-      <p>Hacked by Donuts'n'Legends Co - Daniel Yoshizawa</p>
     </div>
   )
 }
