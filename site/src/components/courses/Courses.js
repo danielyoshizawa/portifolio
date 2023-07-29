3736b221a08726db5e7f5c5d1b71ea0d3fbd2952
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-07-29 13:21:52 -0300

    Adding show more to courses - #37

--- site/src/components/courses/Courses.js
+++ site/src/components/courses/Courses.js
@@ -1,8 +1,10 @@
-import CourseCard from './card/CourseCard'
 import './Courses.css'
+import CourseCard from './card/CourseCard'
+import React, {useState} from 'react'
 
 function Courses (props) {
   const courses = props.courses
+  const [showMore, setShowMore] = useState(false);
   return (
     <div id="certifications-background">
       <div id="certifications">
@@ -10,10 +12,13 @@ function Courses (props) {
         <div className="list-courses" data-testid="list-courses">
           {
             courses.map((item, index) => (
-              <CourseCard index={index} item={item} key={`${index}-course-card`}/>
+              <CourseCard index={index} item={item} key={`${index}-course-card`} showMore={showMore}/>
             ))
           }
         </div>
+      </div>
+      <div className="certifications-show-more">
+        <p className="item" onClick={() => {setShowMore(!showMore)}}>{showMore ? "Show Less" : "Show More"}</p>
       </div>
     </div>
   )
