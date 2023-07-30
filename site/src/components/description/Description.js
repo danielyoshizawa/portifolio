dd30153ae42c2420eceaeb72805fa366eb63456e
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-07-30 17:17:20 -0300

    Consuming api for description - #41

--- site/src/components/description/Description.js
+++ site/src/components/description/Description.js
@@ -1,15 +1,10 @@
 import './Description.css'
 
-function Description() {
+function Description(props) {
   return (
     <div id="about">
-      <h2 className="title-description" data-testid="title-description">Who is Daniel the Software Engineer?</h2>
-      <p className="text-description" data-testid="text-description">
-        Daniel is an experienced software engineer with a diverse background in various areas of software development.<br />
-        With over 14 years in the industry, He has worked with major companies both nationally and internationally. <br />
-        Thorught his carrer, he has been involved in numerous important projects and have gained expertise in a wide variety
-        of thechnologies.
-      </p>
+      <h2 className="title-description" data-testid="title-description">{props.description.title}</h2>
+      <p className="text-description" data-testid="text-description">{props.description.description}</p>
     </div>
   )
 }
