318833d8754cce34f339f07ba8fbb3f48f822bfd
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-07-30 12:21:18 -0300

    Moving app to pages home folder - #40

--- site/src/Routes.js
+++ site/src/Routes.js
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
-import App from './App';
+import Home from './pages/home/Home';
 import Admin from './pages/admin/Admin'
 
 export const routes = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <Home />
   },
   {
     path: "/admin",
