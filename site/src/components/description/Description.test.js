c685009d729f65232870af21889761de81c231a2
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-07-30 18:00:54 -0300

    Adding mock to the test - #41

--- site/src/components/description/Description.test.js
+++ site/src/components/description/Description.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import Description from './Description';
 
+const MOCK_DESCRIPTION = {
+  title: "Test",
+  description : "Test Description"
+}
+
 test('checks if the description has the correct items', () => {
-  render(<Description />)
+  render(<Description description={MOCK_DESCRIPTION}/>)
 
   const title = screen.getByTestId("title-description")
   const text = screen.getByTestId("text-description")
