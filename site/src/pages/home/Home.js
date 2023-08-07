7a6500ad419bfebcb78be90a52d1190f064813c3
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-08-07 11:16:17 -0300

    Getting data from the server - #42

--- site/src/pages/home/Home.js
+++ site/src/pages/home/Home.js
@@ -6,16 +6,13 @@ import WorkExperience from '../../components/workExperience/WorkExperience'
 import Courses from '../../components/courses/Courses'
 import Education from '../../components/education/Education'
 import Footer from '../../components/footer/Footer'
-// Load data
-// TODO : Consume from the server
-import experienceData from '../../data/WorkExperience.data.json'
-
 import {useState, useEffect } from 'react'
 
 function Home() {
   const [description, setDescription] = useState({title: "", description: ""});
   const [education, setEducation] = useState({education: [{}]});
   const [course, setCourse] = useState({course: [{}]});
+  const [workExperience, setWorkExperience] = useState({workExperience: [{ description: "", techs : []}]});
   useEffect(() => {
     fetch("http://localhost:3001/description")
       .then((response) => response.json())
@@ -32,14 +29,20 @@ function Home() {
         .then((json) => {
           setCourse(JSON.parse(json))
       })
+      fetch("http://localhost:3001/workExperience")
+        .then((response) => response.json())
+        .then((json) => {
+          setWorkExperience(JSON.parse(json))
+      })
   }, [])
-
+console.log("HOME")
+console.log(JSON.stringify(workExperience))
   return (
     <>
       <Navbar />
       <Hero />
       <Description description={description} />
-      <WorkExperience experiences={experienceData.experiences} />
+      <WorkExperience experiences={workExperience.workExperience} />
       <Courses courses={course.course} />
       <Education education={education.education} />
       <Footer />
