63058a61f7fea31db027ca7975e114a640ec17e5
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-09-10 10:04:29 -0300

    Adding tags to admin description section - #139

--- site/src/components/admin/description/DescriptionEdit.js
+++ site/src/components/admin/description/DescriptionEdit.js
@@ -13,14 +13,24 @@ function DescriptionEdit(props) {
   const refs =
   {
     title       : useRef(),
-    description : useRef()
+    description : useRef(),
+    tags        : useRef([])
   }
 
   const [status, setStatus] = useState("")
+  const [tags, setTags] = useState([])
   const serverAddress = process.env.REACT_APP_SERVER_ADDRESS
 
   // Load resource from database
   useEffect(() => {
+    // Fetch all the tags
+    fetch(serverAddress + "/tags")
+      .then((response) => response.json())
+      .then((json) => {
+        const resp = JSON.parse(json)
+        setTags(resp.tags)
+      })
+
     // If it's a new entry, we don't need to load from the database
     if (props.action === "new") {
       return
@@ -38,6 +48,18 @@ function DescriptionEdit(props) {
         if (!isEmpty(resp)) {
           refs.title.current.value         = resp.title
           refs.description.current.value   = resp.description
+
+          let tagsMap = new Map()
+          resp.tags && resp.tags.map((item) => {
+            if (item) {
+              tagsMap.set(item.identity, true)
+            }
+          })
+          refs.tags && refs.tags.current.map((item) => {
+            if (item && tagsMap.get(parseInt(item.value))) {
+              item.checked = true
+            }
+          })
         }
       })
   }, [props.action
@@ -45,6 +67,7 @@ function DescriptionEdit(props) {
     , serverAddress
     , refs.title
     , refs.description
+    , refs.tags
   ])
 
   const validateItem = (item) => {
@@ -60,9 +83,17 @@ function DescriptionEdit(props) {
   const handleSubmit = (event) => {
     event.preventDefault()
 
+    let tagsResp = []
+    refs.tags.current.map((item) => {
+      if (item.checked) {
+        tagsResp.push({identity: item.value})
+      }
+    })
+
     const item = {
       title       : refs.title.current.value,
-      description : refs.description.current.value
+      description : refs.description.current.value,
+      tags        : tagsResp
     }
 
     if (!validateItem(item)) return
@@ -107,6 +138,18 @@ function DescriptionEdit(props) {
           type="text"
           ref={refs.description}
         ></textarea>
+        <label>Tags</label>
+        <ul className="admin-description-tag-list">
+        {tags && tags.map((item, index) => {
+          return (
+            <li className="admin-description-tag-item" key={index}>
+              <input type="checkbox" value={item.id} ref={(element) => { refs.tags.current[index] = element }
+              } />
+              <p>{item.id} || {item.name} || {item.type}</p>
+            </li>
+          )
+        })}
+        </ul>
         <input type="submit" value="Save Changes"/>
         <input type="button" value="Cancel" onClick={() => navigate("/admin/description/")} />
       </form>
